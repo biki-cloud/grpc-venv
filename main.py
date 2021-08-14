ca0c5037d64ebc86da2df43fa4093ba8444a8d48
hibiki <hibiki123123@gmail.com>
2021-08-14 18:23:40 +0900

    aaa

---
+++ main.py
@@ -0,0 +1,6 @@
+# python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/helloworld.proto
+
+
+# cd grpc/examples/python/helloworld
+# python3 greeter_server.py
+# python3 greeter_client.py
